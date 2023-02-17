c627d52e6ed56982161c7782004ba936bd523904
ali ubaydullayev <mukhammadali.ubaydullayev.05@inbox.ru>
2023-02-17 07:56:14 +0500

    update

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,18 @@ import '@/styles/globals.css'
 import {Provider} from 'react-redux'
 import store from '../redux/store/index'
 import Message from "@/components/Message";
+import {useEffect} from "react";
+import {useRouter} from "next/router";
 export default function App({ Component, pageProps }) {
+
+  const router = useRouter()
+
+  useEffect(()=> {
+    if(!(localStorage.getItem('accessToken'))) {
+      router.push('/login')
+    }
+  }, [])
+
   return <Provider store={store}>
     <Message />
     <Component {...pageProps} />
